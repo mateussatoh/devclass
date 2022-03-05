91690e83e10432d0fa7288c246d9b3dcf1c99ca2
Mateus Satoh <mateussatoh@gmail.com>
2022-03-05 03:24:21 -0300

    add: theming

--- web/craco.config.js
+++ web/craco.config.js
@@ -7,7 +7,13 @@ module.exports = {
             options: {
                 lessLoaderOptions: {
                     lessOptions: {
-                        modifyVars: { '@primary-color': '#1DA57A' },
+                        modifyVars: {
+                            '@primary-color': '#454158',
+                            "@link-color": "#80FFEA",
+                            "@success-color": "#8AFF80",
+                            "@warning-color": "#FFCA80",
+                            "@error-color": "#FF9580",
+                        },
                         javascriptEnabled: true,
                     },
                 },
